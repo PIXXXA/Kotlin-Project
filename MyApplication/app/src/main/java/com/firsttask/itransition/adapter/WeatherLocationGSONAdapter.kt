5f8e510616c24e81603b5cc12b67962fe376fef1
PIXXXA <itchy.scratchy159@gmail.com>
2019-12-24 16:19:31 +0300

    Test commit DaggerBranch

--- MyApplication/app/src/main/java/com/firsttask/itransition/adapter/WeatherLocationGSONAdapter.kt
+++
@@ -1,8 +0,0 @@
-package com.firsttask.itransition.adapter
-
-import com.google.gson.annotations.SerializedName
-
-class WeatherLocationGSONAdapter {
-    @SerializedName("Key")
-    var key: String? = null
-}
