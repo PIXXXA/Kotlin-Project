bda04b77c65cf7bc3c71d200ed10969c6615d73b
PIXXXA <itchy.scratchy159@gmail.com>
2019-12-25 16:10:35 +0300

    1

---
+++ MyApplication/app/src/main/java/com/firsttask/itransition/dagger/AppModule.kt
@@ -0,0 +1,19 @@
+//package com.firsttask.itransition.dagger
+//
+//import android.content.Context
+//import dagger.Module
+//import dagger.Provides
+//import javax.inject.Singleton
+//
+//@Module
+//class AppModule(private val context: Context){
+//    init {
+//        this.context
+//    }
+//
+//    @Provides
+//    @Singleton
+//    fun provideContext():Context{
+//        return context
+//    }
+//}
---
+++ MyApplication/app/src/main/java/com/firsttask/itransition/dagger/UtilsModule.kt
@@ -0,0 +1,19 @@
+//package com.firsttask.itransition.dagger
+//
+//import androidx.annotation.NonNull
+//import dagger.Module
+//import dagger.Provides
+//import javax.inject.Singleton
+//
+//@Module
+//class UtilsModule() {
+//    @Provides
+//    @NonNull
+//    @Singleton
+//    fun getRestClient() {
+//        return
+//    }
+//
+//
+//
+//}
