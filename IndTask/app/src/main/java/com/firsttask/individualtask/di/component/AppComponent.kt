bc9bf79465dc11f04c6c3c7d84ddd3e081277fd6
a.ushakov <a.ushakov@itransition.com>
2020-02-21 14:13:57 +0300

    First commit

--- IndTask/app/src/main/java/com/firsttask/individualtask/di/component/AppComponent.kt
+++ IndTask/app/src/main/java/com/firsttask/individualtask/di/component/AppComponent.kt
@@ -3,6 +3,7 @@ package com.firsttask.individualtask.di.component
 import com.firsttask.individualtask.di.AppModule
 import com.firsttask.individualtask.di.NetworkModule
 import com.firsttask.individualtask.di.UtilsModule
+import com.firsttask.individualtask.di.ViewModelFactoryModule
 import com.firsttask.individualtask.fragment.history.HistoryFragment
 import com.firsttask.individualtask.fragment.map.MapFragment
 import com.firsttask.individualtask.fragment.weather.WeatherFragment
@@ -10,7 +11,8 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class, UtilsModule::class, NetworkModule::class])
+@Component(modules = [AppModule::class, UtilsModule::class, NetworkModule::class,
+    ViewModelFactoryModule::class])
 interface AppComponent {
     fun inject(historyFragment: HistoryFragment)
     fun inject(weatherFragment: WeatherFragment)
